caa84319f3aaabc024c0d8a62adb48299c87d9da
Ian McIntyre <ianpmcintyre@gmail.com>
2024-04-19 03:59:03 -0400

    Add smoke test for breakpoint persistence
    When a target resets, it should persist its breakpoints. At least that's
    how system resets on the MIMXRT 10xx series seem to work. On the other
    hand, that's not how system resets on the MIMXRT 11xx series would work,
    unless there's custom support in the debug sequence. I used this smoke
    test to help me test that custom support.
    
    I'm re-using the ARM target binary, used for step testing, to realize
    the new test case.

--- smoke-tester/src/tests.rs
+++ smoke-tester/src/tests.rs
@@ -8,7 +8,7 @@ use probe_rs::{
     flashing::{DownloadOptions, FlashProgress, FormatKind, download_file_with_options},
 };
 
-pub mod stepping;
+pub mod test_arm;
 
 use miette::{IntoDiagnostic, Result, WrapErr};
 
